689c86fa074d457697a945a655d2693a6841ead3
Saini Patala <saini.patala@gmail.com>
2017-02-06 18:22:44 +0200

    testproducts mysqlscript added and php connection to database created in fine connect.php

--- Backend/database/testproducts.sql
+++ Backend/database/testproducts.sql
@@ -3,5 +3,5 @@ INSERT INTO product (name, price, description, image_url) VALUES
 ("Aliexpress giftcard", 5000, "20 dollar giftcard to Aliexpress!", "https://alixblog.com/wp-content/uploads/2015/02/Aliexpress-China.png"),
 ("Shower Cap", 400, "This shower cap will keep your hair dry!", "https://s-media-cache-ak0.pinimg.com/originals/f2/74/43/f27443c6ef7a2d6b3d7b4fbdc24da127.jpg"),
 ("Christmas Card", 50, "Merry Christmas!", "http://christmasgator.com/wp-content/uploads/2016/09/christmas-greeting-card-messages-for-teacher.jpg"),
-("Coca-Cola Fridge"), 2000, "A perfect Coca-Cola fridge to always keep a few Cokes cold for you!", "https://i5.walmartimages.com/asr/8be4f68d-4b73-4d7e-ae23-a885586a2761_1.a3c11afed43b10c584682428d033d94b.jpeg"),
+("Coca-Cola Fridge", 2000, "A perfect Coca-Cola fridge to always keep a few Cokes cold for you!", "https://i5.walmartimages.com/asr/8be4f68d-4b73-4d7e-ae23-a885586a2761_1.a3c11afed43b10c584682428d033d94b.jpeg"),
 ("Zalando Giftcard", 10000, "Fashion for you! Get this 40 dollar giftcard to Zalando for only 10000 coins!", "https://pbs.twimg.com/profile_images/705724583794630656/Id7jmjPO.jpg");
