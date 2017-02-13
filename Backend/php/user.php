b4424549e7d324003a79152066be99db96e90d51
Saini Patala <saini.patala@gmail.com>
2017-02-13 17:45:32 +0200

    a few more methods added to user.php class

--- Backend/php/user.php
+++ Backend/php/user.php
@@ -2,6 +2,7 @@
 require '/Users/sainipatala/Documents/PHP/playandwin/rb.php';
 R::setup( 'mysql:host=localhost;dbname=playandwin', 'root', '' );
 
+//Just for testing => remove later
 $user = new user();
 $user->setUser("newuser", "newuser@new.fi", "usernew", "John", "Doe");
 $user->getUser(5);
@@ -17,6 +18,13 @@ class user {
     $user->firstname = $firstname;
     $user->lastname = $lastname;
     $newuser = R::store( $user );
+  }
+
+//This method is used to update an excisting user. So far only description will be updated, but other values will be updated later too
+  function updateUser ($id, $description) {
+    $user = getUser($id);
+    $user->description = $description;
+    R::store($user);
   }
 
 //This method is used to get user data by id
