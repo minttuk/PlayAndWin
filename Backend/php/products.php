74c3b517bd0566ed4138ae13e28d6d147c697628
Tuomas <Tuomas>
2017-02-15 18:01:59 +0200

    Webstore muutokset ja muuta sälää

---
+++ Backend/php/products.php
@@ -0,0 +1,16 @@
+<?php
+require 'rb.php';
+
+ R::setup( 'mysql:host=localhost;dbname=playandwin', 'root', '' );
+
+ /*$user = R::dispense( 'user' );
+
+ $user->username = 'Tuomas';
+ $user->email = 'tuomas@tuomas.com';
+ $user->password = 'salasana';
+
+ R::store( $user );*/
+
+ $products = json_encode(R::getAll( 'SELECT * FROM product' ));
+
+echo $products;
