f4630175da63165150aefa196e4ac0c87a4992a4
Ali Abdollahi <Ali Abdollahi>
2017-03-04 15:57:44 +0200

    Optimized chat to load much quicker by generating html in php as opposed to javascript.

--- Backend/php/getchat.php
+++ Backend/php/getchat.php
@@ -1,7 +1,33 @@
 <?php
+ini_set('display_errors', 1);
 require '../../rb.php';
 R::setup( 'mysql:host=localhost;dbname=playandwin', 'root', '' );
 //if (isset($_SESSION['id'])) {
 $results=R::getAll('select * from chatroom');
-echo json_encode($results);
+$html = '';
+$colorshift = true;
+if (isset($_REQUEST['html'])) {
+  for ($i = max(0,sizeof($results) - 30); $i < sizeof($results); $i++) {
+      $html .=
+          '<div class="media msg" style='.cardColor().'>'.
+          '<div class="media-body">'.
+          '<small class="pull-right time"><i class="fa fa-clock-o"></i>'.' '.$results[$i]['ts'].'</small>'.
+          '<h4 class="media-heading">'.$results[$i]['username'].'</h4>'.
+          '<p class="col-lg-10">'.$results[$i]['msg'].'</p>'.
+          '</div></div>';
+  }
+  echo $html;
+} else {
+  echo json_encode($results);
+}
 //}
+function cardColor() {
+  global $colorshift;
+  if ($colorshift) {
+    $colorshift = !$colorshift;
+    return 'background-color:#d2e2fa;';
+  } else {
+    $colorshift = !$colorshift;
+    return 'background-color:#def7fe;';
+  }
+}
--- Frontend/chat/chat.js
+++ Frontend/chat/chat.js
@@ -5,24 +5,18 @@ $('#chatbox').perfectScrollbar();
 
 $(document).ready(function() {
     setInterval(update, 1000);
-    $.getJSON("../../Backend/php/getchat.php", function(result) {
-        prevResult = result;
-        for (i = Math.max(0,result.length - 30); i < result.length; i++) {
-            $(".msg-wrap").append('' +
-                '<div class="media msg" style='+cardColor()+'>' +
-                '<div class="media-body">' +
-                '<small class="pull-right time"><i class="fa fa-clock-o"></i>' + ' ' + result[i].ts + '</small>' +
-                '<h4 class="media-heading">' + result[i].username + '</h4>' +
-                '<p class="col-lg-10">' + result[i].msg + '</p>' +
-                '</div>' +
-                '</div>'
-            );
-        }
+    $.ajax({url: "../../Backend/php/getchat.php?html",
+    datatype: 'html',
+    success: function(result){
+        $(".msg-wrap").html(result);
         $('#loading').remove();
         $('#padding').remove();
         $('.input-group').css('visibility','visible');
         $('#chatbox').scrollTop($('#chatbox')[0].scrollHeight);
         $('#chatbox').perfectScrollbar('update');
+    }});
+    $.getJSON("../../Backend/php/getchat.php", function(result) {
+        prevResult = result[result.length - 1].id;
     });
 });
 
@@ -37,8 +31,9 @@ function sendMsg() {
 
 function update() {
     $.getJSON("../../Backend/php/getchat.php", function(result) {
-        if (result[result.length - 1].id != prevResult[prevResult.length - 1].id) {
-            prevResult = result;
+        if (result[result.length - 1].id != prevResult) {
+            doOnce = true;
+            prevResult = result[result.length - 1].id;
             $(".msg-wrap").append('' +
                 '<div class="media msg" style='+cardColor()+'>' +
                 '<div class="media-body">' +
