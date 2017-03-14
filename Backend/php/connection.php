c0d6be91d859b341b26041260c901897cb77e2e8
Ali Abdollahi <Ali Abdollahi>
2017-03-14 11:46:05 +0200

    New database connection script working.

--- Backend/php/connection.php
+++ Backend/php/connection.php
@@ -5,28 +5,12 @@ $database = 'playandwin';
 $username = 'root';
 $password = '';
 
-$errorMsg = null;
-
-
 R::setup( 'mysql:host=localhost;dbname='.$database, $username, $password );
 
-$isConnected = R::testConnection();
-
-if (!$isConnected) {
+// Establish database for Jenkins
+if (!R::testConnection()) {
   R::addDatabase('jenkinsDB','mysql:host=10.114.32.140;dbname='.$database, 'jenkins', 'jenkins' );
   R::selectDatabase('jenkinsDB');
 }
 
-
-
-/*
-try {
-  R::setup( 'mysql:host=localhost;dbname='.$database, $username, $password );
-} catch (SQLException $e) {
-  try {
-    R::setup( 'mysql:host=10.114.32.140;dbname='.$database, 'jenkins', 'jenkins' );
-  } catch (SQLException $e1) {
-        e1.printStackTrace();
-    }
-  }*/
 ?>
