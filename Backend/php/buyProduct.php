96cce831b37ba2e8e31c0bcd2dadefdc08fcb963
Ali Abdollahi <ali.abdollahi@metropolia.fi>
2017-03-22 09:29:13 +0200

    Improved the user search algorithm and made some optimizations to buyProduct.php

--- Backend/php/buyProduct.php
+++ Backend/php/buyProduct.php
@@ -16,23 +16,17 @@ function buyProduct(){
     //checks if the user is signed in or not.
     if (isset($_SESSION['id'])) {
         $user = R::load('user',$_SESSION['id']);
-        $user_id = $user->id;
-        $user_coins = R::getCell( 'SELECT coins FROM user WHERE id = :user_id', [':user_id' => $user_id]);
         $product_price = R::getCell( 'SELECT price FROM product WHERE id = :product_id', [':product_id' => $product_id]);
-
         // Does the user have enough coins to buy this product?
-        if ($user_coins<$product_price){
+        if ($user->coins<$product_price){
             echo json_encode(array('message'=>'You cannot buy this yet. You need more coins.'));
-        }
-        //If the user can buy the product:
-        else{
-            //subtract price from user's coins
-            $coins_left = $user_coins-$product_price;
-            R::exec( 'UPDATE user SET coins = :coins_left WHERE id = :user_id', [':coins_left' => $coins_left,':user_id' => $user_id]);
-
-            makeShopOrder($user_id);
+        } else {
+            //If the user can buy the product subtract price from user's coins
+            $user->coins = $user->coins-$product_price;
+            makeShopOrder($user->id);
             makeOrderRow($product_id);
-            addToCollection($product_id, $coins_left,$_SESSION['id'] );
+            addToCollection($product_id, $user->coins,$_SESSION['id'] );
+            R::store($user);
         }
     }
     else {
@@ -58,7 +52,6 @@ function makeShopOrder($user_id){
  */
 function makeOrderRow($product_id){
     R::exec( 'INSERT INTO order_row (order_id, product_id, amount) VALUES (LAST_INSERT_ID(), :product_id, 1)', [':product_id' => $product_id]);
-
 }
 
 /**
@@ -68,31 +61,18 @@ function makeOrderRow($product_id){
  * @param int $coins_left is the amount of coins the user has left after buying the product.
  * @param int $session_id is the id number of the user who has bought the product.
  */
-function addToCollection($product_id, $coins_left, $session_id){
-    //$row = R::getCell( 'SELECT products FROM collection WHERE id = :id', [':id' => $session_id]);
+function addToCollection($product_id, $coins, $session_id){
     $collection = R::load('collection',$session_id);
-
-    //$row will be in form {"product_id":amount, "product_id:amount...}
     $products = json_decode($collection->products, true);
-    //$dataObject = json_decode($row, true);
 
     if ($products == null){
-        //$arr = array("$product_id"=>"1");
         $products = array($product_id=>1);
-    }
-    else if (array_key_exists($product_id, $products)){
-        //$amount = $dataObject[$product_id];
-        //$newAmount = $amount+1;
+    } else if (array_key_exists($product_id, $products)){
         $products[$product_id]++;
-    }
-    else{
+    } else {
         $products[$product_id] = 1;
-        //array_push($dataObject, 1);
     }
-    //$newRow = json_encode($dataObject);
     $collection->products = json_encode($products);
     R::store($collection);
-    //R::exec( 'UPDATE collection SET products = :newRow WHERE id = :id', [':newRow' => $newRow, ':id' => $session_id]);
-
-    echo json_encode(array('message'=>'You have bought this product! You have '.$coins_left.' coins left.' ));
+    echo json_encode(array('message'=>'You have bought this product! You have '.$coins.' coins left.' ));
 }
--- Backend/php/search.php
+++ Backend/php/search.php
@@ -1,6 +1,6 @@
 <?php
 function searchUsers($query) {
-  $results = R::getAll('SELECT * FROM user WHERE username LIKE :q', [':q' => '%'.$query.'%']);
+  $results = R::getAll('SELECT * FROM user WHERE username LIKE :q1 ORDER BY IF( username LIKE :q2 ,0,1)', [':q1' => '%'.$query.'%',':q2'=>$query.'%']);
   $matches = array();
   foreach ($results as $result) {
     $matches[] = array('name'=>$result['username'],'image'=>$result['profilepicture']);
