3ad9087c14a88c94a7ff89da62a4dad06e53c711
Ali Abdollahi <ali.abdollahi@metropolia.fi>
2017-03-22 10:44:49 +0200

    Deleted useless files and folders as well as modified 404 error page

--- Backend/php/getUsers.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'connection.php';
-connect();
-$results = R::getAll('SELECT * FROM user');
-$matches = array();
-foreach ($results as $result) {
-  $matches[] = array('name'=>$result['username'],'image'=>$result['profilepicture']);
-}
