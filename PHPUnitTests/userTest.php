21b8724aa3cd7d0052ac83a20c0568c34408cdcb
Tuomas Paavola <tuomas.paavola@verco.fi>
2017-03-30 00:03:17 +0300

    PHPUnitTest korjaus yritys 14

--- PHPUnitTests/userTest.php
+++ PHPUnitTests/userTest.php
@@ -21,7 +21,7 @@ final class userTest extends TestCase {
   }
 
   public function testgetUserInfo() {
-    $info = getUserInfo(-1);
+    $info = getUserInfo(null);
     $this->assertEquals(array('error'=>'No user found'), $info);
     /*$this->assertArrayHasKey('error', json_decode($info, true));
     $this->assertArrayNotHasKey('username', $info);
