5dae365529447f1fae4dca58504b88999fca6c7f
Ali Abdollahi <ali.abdollahi@metropolia.fi>
2017-03-30 13:03:38 +0300

    Fixed searchTest

--- PHPUnitTests/searchTest.php
+++ PHPUnitTests/searchTest.php
@@ -9,6 +9,6 @@ require_once '../rest/dependencies/require_all.php';
 
 final class searchTest extends TestCase {
   public function searchUsersTest() {
-      $this->assertEquals('Bobby',json_decode(searchUsers('bob'))['name'];
+      $this->assertEquals('Bobby',json_decode(searchUsers('bob'))['name']);
   }
 }
