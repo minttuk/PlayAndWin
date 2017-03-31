c5f314263a7c70fc79a6637f611924c9a740cab3
minttu <minttu.koponen@gmail.com>
2017-04-01 00:22:15 +0300

    tried testAddToCollection agan

--- PHPUnitTests/productTest.php
+++ PHPUnitTests/productTest.php
@@ -49,19 +49,21 @@ final class addProductTest extends TestCase{
     }
 
     //function addToCollection($product_id, $coins_left, $session_id) ->(3,1,1)
-    /*public function testaddToCollection(){
+    public function testaddToCollection(){
         $collection = R::load('collection',1);
-        $products = json_decode($collection->products, true);
+        //$products = json_decode($collection->products, true);
+        $products = $collection->products;
         $amount = $products[3];
         $this->expectOutputString('{"message":"You have bought this product! You have 1 coins left."}', addToCollection('1', '1', '1') );
-        $collection = R::load('collection',1);
-        $products = json_decode($collection->products, true);
-        $newAmount = $products[3];
+        $collection2 = R::load('collection',1);
+        //$products = json_decode($collection->products, true);
+        $products2 = $collection2->products;
+        $newAmount = $products2[3];
         $this->assertEquals($amount+1, $newAmount);
-        $products[3]--;
-        $collection->products = json_encode($products);
-        R::store($collection);
+        $products2[3]--;
+        //$collection2->products = json_encode($products2);
+        R::store($collection2);
 
-    }*/
+    }
 
 }
