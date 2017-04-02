a63325a753bd6212134887c9df88f77fce4eff27
Tuomas Paavola <tuomas.paavola@verco.fi>
2017-04-02 17:02:50 +0300

    PHPUnitTest loginTest.php fixes vol 1

--- PHPUnitTests/loginTest.php
+++ PHPUnitTests/loginTest.php
@@ -6,28 +6,28 @@ echo "xxxxx" . getcwd();
 require_once '../rest/dependencies/require_all.php';
 
 final class loginTest extends TestCase {
-  public function loginUserTest() {
+  public function testloginUser() {
       $this->assertEquals(-1, json_decode(loginUser(null, null, null, null, null, null)));
       $this->assertEquals('[{"data":"Username taken, try again!"}]',loginUser('Bobby', 'moiMoir3', 'moiMoir3', 'trololo@trol.com',null, null));
       $this->assertEquals('[{"data":"Nope, wrong username or password!"}]',loginUser('Fake', 'moiMoir3', 'moiMoir3', null, null, null));
   }
-  public function logOutTest() {
+  public function testlogOut() {
       $this->assertEquals('You have been logged out.', logOut());
   }
-  public function regUserTest() {
+  public function testregUser() {
       $this->assertEquals(true, regUser('Fake', 'moiMoir3', 'moiMoir3', 'testi@testi.com', 'Php', 'UnitTest'));
   }
-  public function findUserTest(){
+  public function testfindUser(){
       $this->assertEquals('Fake', findUser('Fake')->username);
   }
-  public function getUserTest() {
+  public function testgetUser() {
       $this->assertEquals('Fake', getUser(findUser('Fake')->id)->username);
   }
-  public function deleteUserTest() {
+  public function testdeleteUser() {
       $delID = findUser('Fake')->id;
       $this->assertEquals('User '.$delID.' deleted.', deleteUser(2, $delID));
   }
-  public function validatePasswordTest() {
+  public function testvalidatePassword() {
       $this->assertEquals(true, validatePassword('moiMoimoir3','moiMoimoir3'));
   }
 }
