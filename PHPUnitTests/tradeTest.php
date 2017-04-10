d13ee4dd729169243f058223e5684dffb19f23b3
Saini Patala <saini.patala@gmail.com>
2017-04-10 13:55:21 +0300

    a few more tests added to tradeTest

--- PHPUnitTests/tradeTest.php
+++ PHPUnitTests/tradeTest.php
@@ -13,7 +13,7 @@ final class tradeTest extends TestCase {
     $price = 999;
     $desc = 'test';
     // Testing to insert a new trade
-    deleteTradeForTest($user, $product, $price, $desc);
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
     $response = addNewTrade($user, $product, $price, $desc);
     $this->assertEquals('Product has been put for sale!', $response['success']);
     // Testing to insert the same trade twice
@@ -23,7 +23,7 @@ final class tradeTest extends TestCase {
     $response = addNewTrade($user, 1, $price, $desc);
     $this->assertEquals('Oops! You don\'t seem to own this product. Make sure you filled in the right product!', $response['error']);
     // Test trade gets removed
-    deleteTradeForTest($user, $product, $price, $desc);
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
   }
 
   public function testcountOpenTrades() {
@@ -31,12 +31,50 @@ final class tradeTest extends TestCase {
     $product = 5;
     $price = 999;
     $desc = 'test';
-    deleteTradeForTest($user, $product, $price, $desc);
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
     // Counts rows where seller is user and product is 5
     $this->assertEquals(0, countOpenTrades($user, $product));
     $response = addNewTrade($user, $product, $price, $desc);
     $this->assertEquals(1, countOpenTrades($user, $product));
-    deleteTradeForTest($user, $product, $price, $desc);
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
     $this->assertEquals(0, countOpenTrades($user, $product));
   }
+
+  public function testeditTrade() {
+    $user = 1;
+    $product = 5;
+    $price = 999;
+    $newprice = 500;
+    $desc = 'test';
+    $newdesc = 'uusi testi';
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
+    addNewTrade($user, $product, $price, $desc);
+    $trades = getOpenTrades($user);
+    editTrade($user, $trades[0]['id'], $newprice, $newdesc);
+    $trades = getOpenTrades($user);
+    $this->assertEquals($newprice, $trades[0]['price']);
+    $this->assertEquals($newdesc, $trades[0]['description']);
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
+  }
+
+  public function testdeleteTrade() {
+    $user = 1;
+    $product = 5;
+    $price = 999;
+    $desc = 'test';
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
+    addNewTrade($user, $product, $price, $desc);
+    $trades = getOpenTrades($user);
+    $this->assertEquals(1, countOpenTrades($user, $product));
+    deleteTrade($user, $trades[0]['id']);
+    $this->assertEquals(0, countOpenTrades($user, $product));
+    R::exec('DELETE FROM trades WHERE seller_id = :id', [':id' => $user]);
+  }
+
+  public function testuserHasProduct() {
+    $user = 1;
+    $this->assertEquals(false, userHasProduct($user, 1));
+    $this->assertEquals(true, userHasProduct($user, 5));
+  }
+
 }
