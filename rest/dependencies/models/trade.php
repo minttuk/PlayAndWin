1cc3b63ac2b8b6b0e6b4e669d459b353b436721b
Saini Patala <saini.patala@gmail.com>
2017-05-05 09:09:30 +0300

    countrades uncommented

--- rest/dependencies/models/trade.php
+++ rest/dependencies/models/trade.php
@@ -65,13 +65,13 @@ function notAlreadyForTrade($userid, $productid) {
 *@param userid is the current user and productid is the id of the product
 *@return int count of trades
 */
-/*
+
 function countOpenTrades($userid, $productid) {
   $result = R::getAll('SELECT COUNT(*) AS count FROM trades WHERE seller_id = :userid AND product_id = :productid
     AND buyer_id IS NULL', [':userid' => $userid, ':productid' => $productid]);
   return $result[0]['count'];
 }
-*/
+
 /**
 * Returns all trade rows where the current user is either a seller or buyer
 *
