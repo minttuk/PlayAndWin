4462e229e9e0b0ad76ff4470a9d6add8fb834194
Ali Abdollahi <ali.abdollahi@metropolia.fi>
2017-05-08 12:22:25 +0300

    Added phpDocs to authentication.php

--- rest/dependencies/models/authentication.php
+++ rest/dependencies/models/authentication.php
@@ -1,6 +1,15 @@
 <?php
 
 $key = "5d439612ba3cffe8c801acbbde6e4277";
+
+
+/**
+ * Generate a JWT (JSON Web Token) cointaining the user's name and and id
+ * and set it's expiration date for two weeks
+ * @param int $id the ID number of the user
+ * @param string $name the username of the user
+ * @return string JSON Web Access Token
+ */
 function generateToken($id,$name){
   global $key;
   $token = array(
@@ -18,6 +27,10 @@ function generateToken($id,$name){
   return $jwt;
 }
 
+/**
+ * Validate a token present as a cookie or as a request header
+ * @return the id of the validated user
+ */
 function validateToken(){
   if (isset($_COOKIE['access_token'])) $jwt = $_COOKIE['access_token'];
   else if (isset(getallheaders()['access_token'])) $jwt = getallheaders()['access_token'];
